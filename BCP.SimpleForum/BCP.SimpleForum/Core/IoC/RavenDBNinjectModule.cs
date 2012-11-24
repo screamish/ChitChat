36803ec860ae8d59eca1cef339086b47fa46000d
Simon Fenton <swfenton@gmail.com>
2012-11-24 16:54:45 +0000

    Added the solution

---
+++ BCP.SimpleForum/BCP.SimpleForum/Core/IoC/RavenDBNinjectModule.cs
@@ -0,0 +1,30 @@
+﻿using Ninject.Modules;
+using Raven.Client;
+using Raven.Client.Embedded;
+using Raven.Database.Server;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Ninject;
+using Ninject.Web.Common;
+
+namespace BCP.SimpleForum.Core.IoC
+{
+    public class RavenDBNinjectModule : NinjectModule
+    {
+        public override void Load()
+        {
+            Bind<IDocumentStore>()
+           .ToMethod(context =>
+           {
+               NonAdminHttp.EnsureCanListenToWhenInNonAdminContext(8080);
+               var documentStore = new EmbeddableDocumentStore { DataDirectory = "App_Data", UseEmbeddedHttpServer = true, };
+               return documentStore.Initialize();
+           })
+           .InSingletonScope();
+
+            Bind<IDocumentSession>().ToMethod(context => context.Kernel.Get<IDocumentStore>().OpenSession()).InRequestScope();
+        }
+    }
+}
