147efb5e30f8a83c4a430035df044433a6837d1e
Simon Fenton <swfenton@gmail.com>
2012-11-29 01:04:38 +0000

    Adding a profile controller
    Playing around with authorisation and user profiles.

---
+++ BCP.SimpleForum/BCP.SimpleForum/Controllers/ProfileController.cs
@@ -0,0 +1,29 @@
+﻿using BCP.SimpleForum.Models;
+using Raven.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BCP.SimpleForum.Controllers
+{
+    public class ProfileController : Controller
+    {
+        private IDocumentSession _session;
+
+        public ProfileController(IDocumentSession session)
+        {
+            _session = session;
+        }
+
+        [Authorize]
+        [HttpGet]
+        public ActionResult Index()
+        {
+            var user = _session.Load<User>(User.Identity.Name);
+
+            return View(user);
+        }
+    }
+}
